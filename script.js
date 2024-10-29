714b05163465d6095752893ca9ba6b67f8a6d484
Veronica <codebyveronica@gmail.com>
2024-10-29 10:25:21 -0300

    update script.js

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ const month = document.querySelector('#month');
 const year = document.querySelector('#year');
 const calculator = document.querySelector('#calculator')
 
+const yearsTxt = document.querySelector('#yearsTxt');
+const monthsTxt = document.querySelector('#monthsTxt');
+const daysTxt = document.querySelector('#daysTxt');
+
 const currentYear = new Date().getFullYear();
+
+let isValidYear = false;
+let isValidMonth = false;
+let isValidDay = false;
 
 // Functions
 const displayErrorFeedback = (feedbackElement, element, label, message) => {
@@ -30,28 +38,38 @@ const checkDate = (day, month, year) => {
 
   if(day.value === '') {
     displayErrorFeedback(dayFeedbackElement, day, dayLabel, 'This field is required');
+    isValidDay = false;
   }else if(day.value > 31 || day.value == 0) {
     displayErrorFeedback(dayFeedbackElement, day, dayLabel, 'Must be a valid day');
+    isValidDay = false;
   }else if(day.value > daysInMonth(year.value, month.value)) {
     displayErrorFeedback(dayFeedbackElement, day, dayLabel, 'Must be a valid date');
+    isValidDay = false;
   }else {
     removeErrorFeedback(dayFeedbackElement, day, dayLabel);
+    isValidDay = true;
   }
 
   if(month.value === '') {
     displayErrorFeedback(monthFeedbackElement, month, monthLabel, 'This field is required');
+    isValidMonth = false;
   }else if(month.value > 12 || month.value <= 0) {
     displayErrorFeedback(monthFeedbackElement, month, monthLabel, 'Must be a valid month');
+    isValidMonth = false;
   }else {
     removeErrorFeedback(monthFeedbackElement, month, monthLabel)
+    isValidMonth = true;
   }
 
   if(year.value === '') {
     displayErrorFeedback(yearFeedbackElement, year, yearLabel, 'This field is required');
+    isValidYear = false;
   }else if(year.value > currentYear || year.value <= 0) {
     displayErrorFeedback(yearFeedbackElement, year, yearLabel, 'Must be in the past');
+    isValidYear = false;
   }else {
     removeErrorFeedback(yearFeedbackElement, year, yearLabel);
+    isValidYear = true;
   }
 }
 
@@ -73,10 +91,6 @@ const calculateAge = (day, month, year) => {
     ageMonths += 12;
   }
 
-  const yearsTxt = document.querySelector('#yearsTxt');
-  const monthsTxt = document.querySelector('#monthsTxt');
-  const daysTxt = document.querySelector('#daysTxt');
-
   yearsTxt.innerHTML = ageYears;
   monthsTxt.innerHTML = ageMonths;
   daysTxt.innerHTML = ageDays;
@@ -85,5 +99,12 @@ const calculateAge = (day, month, year) => {
 // Event Listeners
 calculator.addEventListener('click', (e) => {
   checkDate(day, month, year);
-  calculateAge(day, month, year);
+
+  if(isValidDay == true && isValidMonth == true && isValidYear == true){
+    calculateAge(day, month, year);
+  } else {
+    yearsTxt.innerHTML = '--';
+    monthsTxt.innerHTML = '--';
+    daysTxt.innerHTML = '--';
+  }
 })
